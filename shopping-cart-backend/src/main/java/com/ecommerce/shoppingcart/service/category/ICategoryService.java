b08e42c77baf22062622da85ddf6e7964e1819de
Abhishek Mishra <abhishekmishra9051@gmail.com>
2024-09-30 07:31:26 +0530

    Implemented the category service

---
+++ shopping-cart-backend/src/main/java/com/ecommerce/shoppingcart/service/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.shoppingcart.service.category;
+
+import java.util.List;
+
+import com.ecommerce.shoppingcart.model.Category;
+
+public interface ICategoryService {
+	Category getCategoryById(Long id);
+	Category getCategoryByName(String name);
+	List<Category> getAllCategories();
+	Category addCategory(Category category);
+	Category updateCategory(Category category, Long id);
+	void deleteCategoryById(Long id);
+
+}
