7ebddbed30047ed23f5d3b08fd30223b205ab6ac
Abhishek Mishra <abhishekmishra9051@gmail.com>
2026-03-22 23:41:13 +0530

    Added video streaming on demand Spring Boot project

---
+++ video-streamig-on-demand/src/main/java/com/abhishek/video_streamig_on_demand/VideoStreamigOnDemandApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.video_streamig_on_demand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamigOnDemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamigOnDemandApplication.class, args);
+	}
+
+}
---
+++ video-streamig-on-demand/src/test/java/com/abhishek/video_streamig_on_demand/VideoStreamigOnDemandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.video_streamig_on_demand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamigOnDemandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
